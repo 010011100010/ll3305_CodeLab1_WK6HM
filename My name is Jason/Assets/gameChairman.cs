c91d79836e7e8892792856019d03fa15642e01f9
Unknown <ll3305@nyu.edu>
2017-03-01 14:21:07 -0500

    Bulit a library

---
+++ My name is Jason/Assets/gameChairman.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+
+public class gameChairman : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		JSONClass json = new JSONClass ();
+		json ["x"].AsFloat = 7;
+		json ["y"].AsFloat = 0;
+		json ["z"].AsFloat = 2;
+		Debug.Log (json);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ My name is Jason/Assets/utilityScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using SimpleJSON;
+
+public class utilityScript : MonoBehaviour {
+
+	public static void WriteJSONToFile(string path, string fileName, JSONClass json){
+		WriteStringToFile (path, fileName, json.ToString ());
+	}
+
+
+	public static string WriteStringToFile (string path, string fileName, string content) {
+		StreamWriter sw = new StreamWriter (path + "/" + fileName);
+		sw.Write(content);
+
+		sw.Close();
+	}
+
+	public static JSONNode ReadJSONFromFile(string path, string fileName){
+		JSONNode result = JSON.Parse (ReadStringFromFile (path, fileName));
+		return result;
+	}
+
+	public static string ReadStringFromFile(string path,string fileName){
+		StreamReader sr = new StreamReader(path + "/" + fileName);
+		string result = sr.ReadToEnd();
+		sr.Close();
+		return result;
+	}
+
+	public static Vector3 CloneVector3(Vector3 vec){
+	
+		return new Vector3 (vec.x, vec.y, vec.z);
+
+	}
+
+}
